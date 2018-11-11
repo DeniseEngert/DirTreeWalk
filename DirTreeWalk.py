b7461d70c19da698a342a255a8eaed36adfffbd7
deniseengert <engertden@gmail.com>
2018-11-11 13:58:13 +0100

    add handling optional arguments

--- DirTreeWalk.py
+++ DirTreeWalk.py
@@ -5,8 +5,9 @@ from os.path import join
 
 directoryStack = []
 
+
 def walkTheTree(htap):
-    # prevent recursion if directory is linked in directory over and over again
+    # prevent recursion if directory contains a link to itself
     real_path = os.path.realpath(htap)
     if real_path in directoryStack:
         return
@@ -16,13 +17,12 @@ def walkTheTree(htap):
         try:
             for item in os.listdir(htap):
                 fil = join(htap, item)
-                real_fil = os.path.realpath(fil) # if file is sym-link, get real file
                 try:
-                    if os.path.isfile(real_fil):
+                    if os.path.isfile(fil):
                         md5hash = hashlib.md5(open(fil, 'rb').read()).hexdigest()
                         print("File-Name: ", item, "\tDirectory: ", htap, "\tHash-Sum: ", md5hash)
                     elif os.path.islink(fil):
-                        print("Directory-Name: ", fil, " , is Sym-Link")
+                        print("Directory-Name: ", fil, " , is Symlink")
                         walkTheTree(fil)
                     elif os.path.isdir(fil):
                         print("Directory-Name: ", fil)
@@ -36,14 +36,21 @@ def walkTheTree(htap):
                                '\nFor the life of me I cannot figure out what it is, '
                                '\nbut it happened here:', htap)
     elif os.path.isfile(htap):
-        print("\033[95m" + "This is a file!")
+        print("\033[95m" + "This is a file, not a directory!")
     else:
         print("\033[95m" + "Sorry, this directory does not exist!")
 
     directoryStack.pop()
 
 
+def optionalArguments(arg):
+    print(arg, " Sorry, I am not very helfful")
+
+
 if __name__ == '__main__':
-    testdir = "/Users/deniseengert/Studium/7FS_ooScriptsprachen/Testdirectory"
-    walkTheTree(testdir)
-    # walkTheTree(sys.argv[1])
+    # testdir = "/Users/deniseengert/Studium/7FS_ooScriptsprachen/Testdirectory"
+    # walkTheTree(testdir)
+    if len(sys.argv) == 3:
+        optionalArguments(sys.argv[1])
+    else:
+        walkTheTree(sys.argv[1])
